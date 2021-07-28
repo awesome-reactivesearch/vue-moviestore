bc0c0177cd9c9984d53161e02030ac5dcf08c630
lohi212 <suvvarilohita@gmail.com>
2021-07-28 14:51:31 +0530

    preload images

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Movie Store' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
