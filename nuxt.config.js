f656967ab2d0f62c13fe8843b503b7642ad68440
lohi212 <suvvarilohita@gmail.com>
2021-07-30 19:10:35 +0530

    feat(step-8): add browser detect plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,8 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/device',
+    { src: '~/plugins/browser-detect', ssr: false },
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
